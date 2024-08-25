7bac4394b63daf27e578f1f7fed145978963242b
Intermaqu <70236348+Intermaqu@users.noreply.github.com>
2024-08-25 14:05:59 +0200

    catch request and handle error

--- Frontend/src/services/api/saltRequest.ts
+++ Frontend/src/services/api/saltRequest.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
 export const saltRequest = async (email: string) => {
-  const response = await axios.post("http://localhost:8000/api/salt", {
-    email: email,
-  });
+  const response = await axios
+    .post("http://localhost:8000/api/salt", {
+      email: email,
+    })
+    .catch((error) => {
+      return error.response;
+    });
 
   return response.data;
 };
--- Frontend/src/services/api/userRequest.ts
+++ Frontend/src/services/api/userRequest.ts
@@ -6,22 +6,32 @@ export const registerUserRequest = async (
   salt: string,
   publicKey: string
 ) => {
-  const response = await axios.post("http://localhost:8000/api/users/create", {
-    email: email,
-    password: password,
-    salt: salt,
-    roles: ["ROLE_USER"],
-    public_key: publicKey,
-  });
+  const response = await axios
+    .post("http://localhost:8000/api/users/create", {
+      email: email,
+      password: password,
+      salt: salt,
+      roles: ["ROLE_USER"],
+      public_key: publicKey,
+    })
+    .catch((error) => {
+      console.error(error.response);
+      return error.response;
+    });
 
   return response.data;
 };
 
 export const loginUserRequest = async (email: string, password: string) => {
-  const response = await axios.post("http://localhost:8000/login", {
-    email: email,
-    password: password,
-  });
+  const response = await axios
+    .post("http://localhost:8000/login", {
+      email: email,
+      password: password,
+    })
+    .catch((error) => {
+      console.error(error.response);
+      return error.response;
+    });
 
   return response.data;
 };
