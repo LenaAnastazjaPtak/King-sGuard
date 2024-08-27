720f8ccd089edd977088e4cfd9a533923a33d9ce
LenaAnastazjaPtak <lena.anastazja.ptak@gmail.com>
2024-08-27 18:15:17 +0200

    feat: CustomAuthenticator.php with check hashing with 'pbkdf2' algorithm

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -7,10 +7,11 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-class User implements UserInterface
+class User implements UserInterface,PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
